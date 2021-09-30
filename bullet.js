b4113a2f451f3a3c4146304c85fb7ea8af76b367
Atharva Mishra <63883108+YamiAtem@users.noreply.github.com>
2021-09-30 20:02:02 +0530

    Class 162
    Co-authored-by: PriyaChadha <PriyaChadha@users.noreply.github.com>

--- bullet.js
+++ bullet.js
@@ -30,11 +30,47 @@ AFRAME.registerComponent('bullet', {
                 var direction = new THREE.Vector3();
                 camera.getWorldDirection(direction)
 
-                bullet.setAttribute("velocity", direction.multiplyScalar(-10))
+                bullet.setAttribute("velocity", direction.multiplyScalar(-75))
 
                 var scene = document.querySelector("#scene")
+
+                bullet.setAttribute("dynamic-body", {
+                    shape: "sphere",
+                    mass: 0
+                });
+
+                bullet.addEventListener("collide", this.remove_bullet)
+                
                 scene.appendChild(bullet)
             }
         });
+    },
+
+    remove_bullet: function (e) {
+        //original entity
+        console.log(e.detail.target.el)
+
+        //other entities which the bullet is touching
+        console.log(e.detail.body.el)
+
+        var element = e.detail.target.el
+        var element_hit = e.detail.body.el
+
+        if (element_hit.id.includes("box")) {
+            element_hit.setAttribute("material", {
+                opacity: 1,
+                transparent: true
+            })
+        }
+
+        var impulse = new CANNON.Vec3(-2, 2, 1)
+        var world_point = new CANNON.Vec3().copy(element_hit.getAttribute("position"))
+
+        element_hit.body.applyImpulse(impulse, world_point)
+
+        element.removeEventListener("collide", this.shoot_bullet)
+
+        var scene = document.querySelector("#scene")
+        scene.removeChild(element)
     }
 })
